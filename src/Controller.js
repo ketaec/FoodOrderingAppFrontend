2bf9602de74347810c6e96a87790955283590257
KetaEs <eswar-chand.keta@basf.com>
2021-07-09 06:29:43 +0200

    added controller js and configured routes

---
+++ src/Controller.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './screens/home/Home';
+
+class Controller extends Component {
+
+    constructor() {
+      super();
+    }
+
+    // controller renderer function with routes configuration
+    render() {
+        return (
+            <Router>
+                <div className="main-container">
+                    <Route exact path='/' render={(props) => <Home {...props} />} />
+                </div>
+            </Router>
+        )
+    }
+}
+
+export default Controller;
