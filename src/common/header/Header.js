4e2d4ff2cca9f0a6c8c47ea69d82c5811f9e4628
KetaEs <eswar-chand.keta@basf.com>
2021-07-09 06:30:18 +0200

    added home and header components

---
+++ src/common/header/Header.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import { Input, InputAdornment } from "@material-ui/core";
+import './Header.css';
+
+class Header extends Component {
+        //header renderer function
+    render() {
+        return (
+            <div className="app-header">
+                Header
+            </div>
+        )
+    }
+}
+
+export default Header;
