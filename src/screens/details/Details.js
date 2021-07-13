abf045aa6e31c0bb20b5eebab7a0ec51073cc6ec
KetaEs <eswar-chand.keta@basf.com>
2021-07-13 11:10:26 +0200

    get restaurant details and render the data in UI

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import './Details.css';
 import Header from '../../common/header/Header';
-import { withStyles } from '@material-ui/core/styles';
-
+import 'font-awesome/css/font-awesome.min.css';
+import Typography from '@material-ui/core/Typography';
 
 
 class Details extends React.Component {
     constructor(props) {
         super(props);
-
+        this.state = {
+            restaurantDetails: {},
+            categories:[],
+            locality: ""
+        }
     }
 
     componentDidMount() {
-        console.log(this.props.location.pathname);
         let {id} = this.props.match.params;
-        console.log(id);
+        this.getRestaurantDetails(id);
+    }
 
+    getRestaurantDetails = (id) => {
+        let that = this;
+        let dataRestaurants = null;
+        let xhrRestaurants = new XMLHttpRequest();
+        xhrRestaurants.addEventListener('readystatechange', function () {
+            if (this.readyState === 4) {
+                let responseText = JSON.parse(this.responseText);
+                that.setState({
+                    restaurantDetails: responseText, 
+                    categories: responseText.categories,
+                    locality: responseText.address.locality});
+            }
+        })
+        xhrRestaurants.open('GET', this.props.baseUrl + '/api/restaurant/' + id);
+        xhrRestaurants.send(dataRestaurants);
     }
 
     render() {
@@ -26,8 +45,38 @@ class Details extends React.Component {
                     showSearch="false"
                     searchHandler={this.searchHandler}
                 />
-                <div>
-                    details page
+                {/* restaurant information */}
+                <div className="restaurant-information">
+                    <div className="restaurant-image">
+                        <div>
+                        <img
+                            className="img-div"
+                            src={this.state.restaurantDetails.photo_URL}
+                            alt='restaurant'
+                            width='100%'
+                        />
+                        </div>
+                    </div>
+                    <div className="restaurant-details">
+                        <div>
+                            <Typography variant="h4" gutterBottom> {this.state.restaurantDetails.restaurant_name} </Typography> 
+                            <Typography variant="h5" gutterBottom> {this.state.locality} </Typography> <br />
+                            <Typography variant="body1" gutterBottom> {this.state.categories
+                                && Array.isArray(this.state.categories)
+                                && this.state.categories.length > 0
+                                && this.state.categories.map((el) => el.category_name).join(", ")} </Typography>
+                        </div>
+                        <div style={{ float: 'left', display: "flex", flexDirection: "row", width: "100%", paddingTop: "20px" }}>
+                            <div style={{ width: "100%" }}>
+                                <i className="fa fa-star" aria-hidden="true"> <span style={{ fontWeight: '500' }}>{this.state.restaurantDetails.customer_rating}</span> </i> <br />
+                                <Typography variant="caption" gutterBottom> AVERAGE RATING BY <br /> <span style={{ fontWeight: 'bold' }}> {this.state.restaurantDetails.number_customers_rated} </span> USERS </Typography>
+                            </div>
+                            <div style={{ width: "100%" }}>
+                                <i className="fa fa-inr" aria-hidden="true"> <span style={{ fontWeight: '500' }}>{this.state.restaurantDetails.average_price}</span> </i> <br />
+                                <Typography variant="caption" gutterBottom> AVERAGE COST FOR <br /> TWO PEOPLE </Typography>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
