a1595819e50368bf570e8cfd41b7f5065c2f90a2
KetaEs <eswar-chand.keta@basf.com>
2021-07-16 08:07:49 +0200

    added stepper in checkout page

--- src/screens/checkout/Checkout.js
+++ src/screens/checkout/Checkout.js
@@ -2,14 +2,50 @@ import React, {Component, Fragment} from 'react';
 import {Redirect} from 'react-router-dom';
 import './Checkout.css';
 import Header from "../../common/header/Header";
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from "@material-ui/core/StepContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 class Checkout extends Component {
     constructor() {
         super();
+        this.state = {
+            activeStep: 0,
+            payments: [],
+            displayChange: 'display-none',
+            selectedAddressId: "clear address",
+            paymentId: "clear paymentid",
+        }
     }
 
     componentDidMount() { 
         console.log(this.props.location.state);
+    }
+
+    incrementActiveStep = () => {
+        if (this.state.activeStep === 0 && this.state.selectedAddressId === undefined) {
+            //Do nothing as it is mandatory to select an address
+        } else if (this.state.activeStep === 1 && this.state.paymentId === '') {
+            //Do nothing, Because user has to select payment to proceed further.
+        } else {
+            let activeState = this.state.activeStep + 1;
+            let changeAddressPayment = 'display-none';
+            if (activeState === 2) {
+                changeAddressPayment = 'display-block';
+            }
+            this.setState({activeStep: activeState, displayChange: changeAddressPayment})
+        }
+    }
+
+    decrementActiveStep = () => {
+        this.setState({activeStep: this.state.activeStep - 1});
+    }
+
+    resetActiveStep = () => {
+        this.setState({activeStep: 0, displayChange: 'display-none'})
     }
 
     render () {
@@ -19,8 +55,43 @@ class Checkout extends Component {
         return (
             <div>
                 <Header baseUrl={this.props.baseUrl} showSearch="false" />
-                <div>
-                    checkout page
+                <div className='main-container'>
+                    <div className='delivery-payment-section'>
+                        <Stepper activeStep={this.state.activeStep} orientation='vertical'>
+                            <Step key='Delivery'>
+                                <StepLabel>Delivery</StepLabel>
+                                <StepContent>
+                                    <div>
+                                        Address
+                                    </div>
+
+
+                                    <div>
+                                        <Button disabled={this.state.activeStep === 0}>Back</Button>
+                                        <Button className='button' variant="contained" color="primary"
+                                                onClick={this.incrementActiveStep}>Next</Button>
+                                    </div>
+                                </StepContent>
+                            </Step>
+                            <Step key='Payment'>
+                                <StepLabel>Payment</StepLabel>
+                                <StepContent>
+                                    <div id='payment-modes'>
+                                        Payment modes
+                                    </div>
+                                    <Button onClick={this.decrementActiveStep}>Back</Button>
+                                    <Button variant="contained" color="primary"
+                                            onClick={this.incrementActiveStep}>Finish</Button>
+                                </StepContent>
+                            </Step>
+                        </Stepper>
+                        <div className={this.state.displayChange} >
+                            <Typography style={{marginLeft: 40}} variant='h5'>
+                                View the summary and place your order now!
+                            </Typography>
+                            <Button style={{marginLeft: 40, marginTop: 20}} onClick={this.resetActiveStep}>CHANGE</Button>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
